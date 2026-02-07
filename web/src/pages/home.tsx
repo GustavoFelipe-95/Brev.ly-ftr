295522455664e7d17d35eb78a814330078416bc9
GustavoFelipe-95 <zegustavo149@gmail.com>
2026-02-07 11:35:55 -0300

    feat: set up Dockerfile and docker-compose for API and PostgreSQL integration, update environment variables and scripts

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -4,7 +4,7 @@ import { HistoryLinks } from '../components/historyLinks'
 
 export function Home() {
   return (
-    <main className="flex flex-col w-full max-w-[995px] mx-auto p-5 md:items-start md:mt-[8vh] items-center">
+    <main className="flex flex-col w-full max-w-[995px] mx-auto p-5 md:items-start md:mt-[4vh] items-center">
       <img src={LogoBrevLy} alt="Brev.ly Logo" className="w-32 h-32" />
       <div className='flex flex-col w-full gap-6 md:flex-row md:items-start'>
         <NewLink />
